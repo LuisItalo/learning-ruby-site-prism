db54423e690ff75bc01fafd1e0a2375b4787c058
Luis Italo <luisitalomoreirasouza@gmail.com>
2022-09-08 08:56:58 -0300

    mapeando listas

--- features/step_definitions/lista_de_elementos.rb
+++ features/step_definitions/lista_de_elementos.rb
@@ -4,4 +4,8 @@ Quando('mapeio uma tabela') do
     @lista_de_elementos.load
     sleep(2)
     puts @lista_de_elementos.lista.size
+
+    @lista_de_elementos.lista.each do |listas|
+        puts listas.text
+    end
 end
